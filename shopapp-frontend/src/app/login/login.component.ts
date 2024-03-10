8f660b87d83e17bdf42110d2c710a95a6dd4aa11
LppNam <lpphuongnam2301@gmail.com>
2024-03-10 20:26:38 +0700

    simplify some code angular ts

--- shopapp-frontend/src/app/login/login.component.ts
+++ shopapp-frontend/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../services/user.service';
+import { LoginDTO } from '../dtos/user/login.dto';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +10,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  @ViewChild(`loginForm`) registerForm!: NgForm;
+  phone : String;
+  password : String;
 
+  constructor(private router: Router, private userService: UserService)
+  {
+    this.phone = '';
+    this.password = '';
+  }
+
+  onPhoneChange()
+  {
+
+  }
+
+  login()
+  {
+      const loginDTO:LoginDTO = {
+        "phone_number": this.phone,
+        "password" : this.password
+      }
+      this.userService.login(loginDTO).subscribe({
+        next: (response: any) => {
+          if(response && (response.status === 200 || response.status === 201))
+          {
+            //this.router.navigate(['/home']);
+            console.log("ok");
+          }
+        },
+        complete: () => {
+
+        },
+        error: (error: any) => {
+          alert(`Login fail, error: ${error.error.message}`);
+        }
+      });
+  }
 }
